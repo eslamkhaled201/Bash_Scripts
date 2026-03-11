25a79a010380674319de37a97cd3b6e261b4dccc
eslam khaled <eslam_khaled@centos8.com>
2026-03-11 04:45:10 +0000

    Added size Rgx matching for creating LMVs function

--- manage_lvms.sh
+++ manage_lvms.sh
@@ -7,14 +7,22 @@ echo "-------- Welcome to LVM storage manager -------"
 ## 3- resize lvm and related file systems
 ## 4- add check for mounted file systems have 90% usage  
 
+echo "1) list LVMs"
+echo "2) create LVM from volume group"
+echo -e "\n"
 option_num=0
 while [[ $option_num -lt 1 || $option_num -gt 4 ]]; do
     read -p "Enter correct option :"  option_num
 done
 
 create_lvm(){
+    sizeRegx="[1-9][M|G]$"
+    size=""
     read -p "Enter lvm name: " lvm
-    read -p "Enter the LVM size: " size
+    while [[ ! $size =~ $sizeRegx ]]; do
+	    echo -e "Enter LVM size \n Ex: 1G or 2M : "
+    	    read size
+    done
     read -p "Enter the Volume group name: " vgname    
     sudo lvcreate -L $size -n $lvm $vgname 2> /dev/null
 
